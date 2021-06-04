9bbc1a2c9560eda564772917c9bcc2965456157b
Jorik Kloppenburg <78646379+JorikKloppenburg@users.noreply.github.com>
2021-06-04 09:55:02 +0200

    Add files via upload

---
+++ Level13_opdracht_001/script.js
@@ -0,0 +1,36 @@
+var age = 18;
+var isFemale = false;
+var driverStatus = "bob";
+
+// Dit is een leeftijdscheck
+if (age >= 18) {
+
+    console.log("Welkom je bent 18 jaar of ouder!");
+}
+else {
+
+    console.log("Je bent nog geen 18 jaar of ouder, ik wil je verzoeken om buiten te blijven.");
+
+}
+
+// Dit is een geslacht check
+if (isFemale) {
+
+    console.log("Welkom op de ladiesnight");
+}
+else {
+
+    console.log("Helaas je bent niet welkom, dit betreft een ladiesnight.");
+
+}
+
+// Dit is een driver status check
+if (driverStatus == "bob") {
+
+    console.log("Jij bent de bob en mag rijden!");
+}
+else {
+
+    console.log("Jij hebt gedronken en bent niet de bob, je mag niet meer rijden!");
+
+}
